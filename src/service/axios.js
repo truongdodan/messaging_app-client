3af4fed845cd15a121389225786d675451f31d40
dantRuongdo <dodantruong333@gmail.com>
2025-10-17 20:46:56 +0700

    Update base url

--- src/service/axios.js
+++ src/service/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://messaging-app-server-8fox.onrender.com",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
