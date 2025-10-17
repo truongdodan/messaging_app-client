71eaa70e300853120341d4acc55e13e0f82b20cf
dantRuongdo <dodantruong333@gmail.com>
2025-10-17 20:56:13 +0700

    Fix file name typo

--- src/components/GoBackBtn/GoBackBtn.jsx
+++ src/components/GoBackBtn/GoBackBtn.jsx
@@ -1,16 +1,20 @@
-import { ArrowLeftIcon } from 'lucide-react'
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
-import './GoBackBtn.css'
+import { ArrowLeftIcon } from "lucide-react";
+import { useNavigate } from "react-router-dom";
+import "./GobackBtn.css";
 
 const GoBackBtn = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
   return (
-    <div className="icon-container go-back" onClick={() => {navigate(-1);}}>
-        <ArrowLeftIcon />
+    <div
+      className="icon-container go-back"
+      onClick={() => {
+        navigate(-1);
+      }}
+    >
+      <ArrowLeftIcon />
     </div>
-  )
-}
+  );
+};
 
-export default GoBackBtn
+export default GoBackBtn;
